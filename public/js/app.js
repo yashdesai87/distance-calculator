6f1c51cf3add976ab1b24130a66c06e34dbc0fbd
Yash Desai <yash@stadiumgoods.com>
2018-10-19 21:38:07 +0530

    Distance calculator implemented

--- public/js/app.js
+++ public/js/app.js
@@ -50329,6 +50329,51 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
@@ -50348,7 +50393,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             lat_1: null,
             long_1: null,
             lat_2: null,
-            long_2: null
+            long_2: null,
+            distance: 0
         };
     },
 
@@ -50356,6 +50402,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     methods: {
         setPlace1: function setPlace1(place) {
 
+            // Get the values from the places object
             this.street_address_1 = place['formatted_address'];
             this.address_1 = _.get(place, 'address_components');
             var _this = this;
@@ -50374,11 +50421,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 }
             });
 
-            this.lat_1 = _.get(place, 'geometry.location.lat');
-            this.long_1 = _.get(place, 'geometry.location.lng');
+            this.lat_1 = place.geometry.location.lat();
+            this.long_1 = place.geometry.location.lng();
         },
         setPlace2: function setPlace2(place) {
 
+            // Get the values from the places object
             this.street_address_2 = place['formatted_address'];
             this.address_2 = _.get(place, 'address_components');
             var _this = this;
@@ -50397,8 +50445,25 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 }
             });
 
-            this.lat_2 = _.get(place, 'geometry.location.lat');
-            this.long_2 = _.get(place, 'geometry.location.lng');
+            this.lat_2 = place.geometry.location.lat();
+            this.long_2 = place.geometry.location.lng();
+        },
+        calculate_distance: function calculate_distance() {
+
+            // Used Haversine algorithm to calculate the distance
+            var haversine = __webpack_require__(89);
+
+            var start = {
+                latitude: this.lat_1,
+                longitude: this.long_1
+            };
+
+            var end = {
+                latitude: this.lat_2,
+                longitude: this.long_2
+            };
+
+            alert('The distance is ' + haversine(start, end, { unit: 'mile' }).toFixed(2) + ' miles');
         }
     }
 
@@ -50413,17 +50478,35 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "container" }, [
-    _c("div", { staticClass: "row justify-content-center" }, [
-      _c(
-        "div",
-        { staticClass: "col-md-6" },
-        [
-          _c("gmap-autocomplete", { on: { place_changed: _vm.setPlace1 } }),
+    _vm._m(0),
+    _vm._v(" "),
+    _c("div", { staticClass: "row justify-content-center mt-10" }, [
+      _c("div", { staticClass: "col-md-5" }, [
+        _c("form", [
+          _c(
+            "div",
+            { staticClass: "form-row" },
+            [
+              _c("label", { attrs: { for: "inputEmail4" } }, [
+                _vm._v("Start typing an address. Address 1")
+              ]),
+              _vm._v(" "),
+              _c("gmap-autocomplete", {
+                staticClass: "form-control",
+                on: { place_changed: _vm.setPlace1 }
+              })
+            ],
+            1
+          ),
           _vm._v(" "),
-          _c("div", { staticClass: "card mb-3" }, [
-            _vm._m(0),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
+          _c("hr"),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-row" }, [
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputEmail4" } }, [
+                _vm._v("Street Address")
+              ]),
+              _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: {
@@ -50434,19 +50517,37 @@ var render = function() {
                 domProps: {
                   value: _vm.street_address_1 ? _vm.street_address_1 : ""
                 }
-              }),
+              })
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputPassword4" } }, [
+                _vm._v("City")
+              ]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: { type: "text", name: "city", placeholder: "City" },
                 domProps: { value: _vm.city_1 ? _vm.city_1 : "" }
-              }),
+              })
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-row" }, [
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputEmail4" } }, [_vm._v("State")]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: { type: "text", name: "state", placeholder: "State" },
                 domProps: { value: _vm.state_1 ? _vm.state_1 : "" }
-              }),
+              })
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputPassword4" } }, [
+                _vm._v("Zip")
+              ]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
@@ -50455,20 +50556,37 @@ var render = function() {
               })
             ])
           ])
-        ],
-        1
-      ),
+        ])
+      ]),
       _vm._v(" "),
-      _c(
-        "div",
-        { staticClass: "col-md-6" },
-        [
-          _c("gmap-autocomplete", { on: { place_changed: _vm.setPlace2 } }),
+      _c("div", { staticClass: "col-md-2" }),
+      _vm._v(" "),
+      _c("div", { staticClass: "col-md-5" }, [
+        _c("form", [
+          _c(
+            "div",
+            { staticClass: "form-row" },
+            [
+              _c("label", { attrs: { for: "inputEmail4" } }, [
+                _vm._v("Start typing an address. Address 2")
+              ]),
+              _vm._v(" "),
+              _c("gmap-autocomplete", {
+                staticClass: "form-control",
+                on: { place_changed: _vm.setPlace2 }
+              })
+            ],
+            1
+          ),
           _vm._v(" "),
-          _c("div", { staticClass: "card mb-3" }, [
-            _vm._m(1),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
+          _c("hr"),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-row" }, [
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputEmail4" } }, [
+                _vm._v("Street Address")
+              ]),
+              _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: {
@@ -50479,19 +50597,37 @@ var render = function() {
                 domProps: {
                   value: _vm.street_address_2 ? _vm.street_address_2 : ""
                 }
-              }),
+              })
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputPassword4" } }, [
+                _vm._v("City")
+              ]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: { type: "text", name: "city", placeholder: "City" },
                 domProps: { value: _vm.city_2 ? _vm.city_2 : "" }
-              }),
+              })
+            ])
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "form-row" }, [
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputEmail4" } }, [_vm._v("State")]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
                 attrs: { type: "text", name: "state", placeholder: "State" },
                 domProps: { value: _vm.state_2 ? _vm.state_2 : "" }
-              }),
+              })
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "form-group col-md-6" }, [
+              _c("label", { attrs: { for: "inputPassword4" } }, [
+                _vm._v("Zip")
+              ]),
               _vm._v(" "),
               _c("input", {
                 staticClass: "form-control",
@@ -50500,9 +50636,27 @@ var render = function() {
               })
             ])
           ])
-        ],
-        1
-      )
+        ])
+      ])
+    ]),
+    _vm._v(" "),
+    _c("div", { staticClass: "row mt-2" }, [
+      _c("div", { staticClass: "col-md-12 text-center" }, [
+        _c(
+          "button",
+          {
+            staticClass: "btn btn-primary",
+            attrs: { type: "button" },
+            on: {
+              click: function($event) {
+                $event.preventDefault()
+                return _vm.calculate_distance($event)
+              }
+            }
+          },
+          [_vm._v("Calculate Distance")]
+        )
+      ])
     ])
   ])
 }
@@ -50511,16 +50665,8 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card-header" }, [
-      _c("h3", [_vm._v("Start typing an address. Address 1")])
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card-header" }, [
-      _c("h3", [_vm._v("Start typing an address. Address 2")])
+    return _c("div", { staticClass: "row mb-2" }, [
+      _c("h1", [_vm._v("Distance Calculator")])
     ])
   }
 ]
@@ -50538,6 +50684,75 @@ if (false) {
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 86 */,
+/* 87 */,
+/* 88 */,
+/* 89 */
+/***/ (function(module, exports) {
+
+var haversine = (function () {
+  var RADII = {
+    km:    6371,
+    mile:  3960,
+    meter: 6371000,
+    nmi:   3440
+  }
+
+  // convert to radians
+  var toRad = function (num) {
+    return num * Math.PI / 180
+  }
+
+  // convert coordinates to standard format based on the passed format option
+  var convertCoordinates = function (format, coordinates) {
+    switch (format) {
+    case '[lat,lon]':
+      return { latitude: coordinates[0], longitude: coordinates[1] }
+    case '[lon,lat]':
+      return { latitude: coordinates[1], longitude: coordinates[0] }
+    case '{lon,lat}':
+      return { latitude: coordinates.lat, longitude: coordinates.lon }
+    case 'geojson':
+      return { latitude: coordinates.geometry.coordinates[1], longitude: coordinates.geometry.coordinates[0] }
+    default:
+      return coordinates
+    }
+  }
+
+  return function haversine (startCoordinates, endCoordinates, options) {
+    options   = options || {}
+
+    var R = options.unit in RADII
+      ? RADII[options.unit]
+      : RADII.km
+
+    var start = convertCoordinates(options.format, startCoordinates)
+    var end = convertCoordinates(options.format, endCoordinates)
+
+    var dLat = toRad(end.latitude - start.latitude)
+    var dLon = toRad(end.longitude - start.longitude)
+    var lat1 = toRad(start.latitude)
+    var lat2 = toRad(end.latitude)
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+    if (options.threshold) {
+      return options.threshold > (R * c)
+    }
+
+    return R * c
+  }
+
+})()
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = haversine
+}
+
 
 /***/ })
 /******/ ]);
--- resources/js/components/AddressForm.vue
+++ resources/js/components/AddressForm.vue
@@ -1,34 +1,79 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                    <gmap-autocomplete
-                        @place_changed="setPlace1">
-                    </gmap-autocomplete>
-                <div class="card mb-3">
-                    <div class="card-header"><h3>Start typing an address. Address 1</h3></div>
-                    <div class="card-body">
-                       <input type="text" name="address" placeholder="Street Address" class="form-control" :value=" street_address_1 ? street_address_1: ''">
-                       <input type="text" name="city" placeholder="City" class="form-control" :value="city_1 ? city_1: ''">
-                       <input type="text" name="state" placeholder="State" class="form-control" :value="state_1 ? state_1  : ''">
-                       <input type="text" name="zip" placeholder="Zip" class="form-control" :value="zip_1 ? zip_1: ''">
+        <div class="row mb-2">
+            <h1>Distance Calculator</h1>
+        </div>
+        <div class="row justify-content-center mt-10">
+            <div class="col-md-5">
+                <form>
+                    <div class="form-row">
+                            <label for="inputEmail4">Start typing an address. Address 1</label>
+                        <gmap-autocomplete
+                            class="form-control"
+                            @place_changed="setPlace1">
+                        </gmap-autocomplete>
                     </div>
-                </div>
+                    <hr>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Street Address</label>
+                            <input type="text" name="address" placeholder="Street Address" class="form-control" :value=" street_address_1 ? street_address_1: ''">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">City</label>
+                            <input type="text" name="city" placeholder="City" class="form-control" :value="city_1 ? city_1: ''">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">State</label>
+                            <input type="text" name="state" placeholder="State" class="form-control" :value="state_1 ? state_1  : ''">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Zip</label>
+                            <input type="text" name="zip" placeholder="Zip" class="form-control" :value="zip_1 ? zip_1: ''">
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="col-md-6">
-                    <gmap-autocomplete
-                        @place_changed="setPlace2">
-                    </gmap-autocomplete>
-                <div class="card mb-3">
-                    <div class="card-header"><h3>Start typing an address. Address 2</h3></div>
-                    <div class="card-body">
-                         <input type="text" name="address" placeholder="Street Address" class="form-control" :value=" street_address_2 ? street_address_2: ''">
-                       <input type="text" name="city" placeholder="City" class="form-control" :value="city_2 ? city_2: ''">
-                       <input type="text" name="state" placeholder="State" class="form-control" :value="state_2 ? state_2  : ''">
-                       <input type="text" name="zip" placeholder="Zip" class="form-control" :value="zip_2 ? zip_2: ''">
-                    </div>
-                </div>
+            <div class="col-md-2"></div>
 
+            <div class="col-md-5">
+                <form>
+                    <div class="form-row">
+                        <label for="inputEmail4">Start typing an address. Address 2</label>
+                        <gmap-autocomplete
+                            class="form-control"
+                            @place_changed="setPlace2">
+                        </gmap-autocomplete>
+                    </div>
+                    <hr>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Street Address</label>
+                            <input type="text" name="address" placeholder="Street Address" class="form-control" :value=" street_address_2 ? street_address_2: ''">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">City</label>
+                            <input type="text" name="city" placeholder="City" class="form-control" :value="city_2 ? city_2: ''">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">State</label>
+                            <input type="text" name="state" placeholder="State" class="form-control" :value="state_2 ? state_2  : ''">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Zip</label>
+                            <input type="text" name="zip" placeholder="Zip" class="form-control" :value="zip_2 ? zip_2: ''">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-md-12 text-center">
+                <button type="button" @click.prevent="calculate_distance" class="btn btn-primary">Calculate Distance</button>
             </div>
         </div>
     </div>
@@ -53,7 +98,8 @@ import * as VueGoogleMaps from "vue2-google-maps";
                 lat_1: null,    
                 long_1: null,    
                 lat_2: null,    
-                long_2: null,    
+                long_2: null,
+                distance: 0,    
             };
         },
 
@@ -62,6 +108,7 @@ import * as VueGoogleMaps from "vue2-google-maps";
                 
             setPlace1(place) {
               
+              // Get the values from the places object
                 this.street_address_1 = place['formatted_address'];
                this.address_1 = _.get(place, 'address_components');
                 let _this = this;
@@ -80,12 +127,13 @@ import * as VueGoogleMaps from "vue2-google-maps";
                     }
                 });
 
-                this.lat_1 = _.get(place, 'geometry.location.lat');
-                this.long_1 = _.get(place, 'geometry.location.lng');
+                this.lat_1 = place.geometry.location.lat();
+                this.long_1 = place.geometry.location.lng();
             },
 
              setPlace2(place) {
                 
+                // Get the values from the places object
                 this.street_address_2 = place['formatted_address'];
                this.address_2 = _.get(place, 'address_components');
                 let _this = this;
@@ -104,10 +152,30 @@ import * as VueGoogleMaps from "vue2-google-maps";
                     }
                 });
 
-                this.lat_2 = _.get(place, 'geometry.location.lat');
-                this.long_2 = _.get(place, 'geometry.location.lng');
+                this.lat_2 = place.geometry.location.lat();
+                this.long_2 = place.geometry.location.lng();
             },
-        }
+
+            calculate_distance(){
+               
+               // Used Haversine algorithm to calculate the distance
+               const haversine = require('haversine')
+
+                let start = {
+                    latitude: this.lat_1,
+                    longitude: this.long_1
+                }
+
+                let end = {
+                    latitude: this.lat_2,
+                    longitude: this.long_2
+                }
+
+
+               alert('The distance is '+ haversine(start, end,{unit: 'mile'}).toFixed(2)+' miles');
+
+                }
+            }
 		
     }
 </script>
